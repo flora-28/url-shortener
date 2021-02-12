d098c6f1c8a74d704dc144a08e513b51fd5ffe8e
Flora Peng <berry1003@outlook.com>
2021-02-12 21:18:24 +0800

    feat: add mongoose

---
+++ models/url.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortenedUrl: {
+    type: String,
+    required: true,
+  }
+})
+
+module.exports = mongoose.model('URL', urlSchema)
