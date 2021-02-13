fa9e2956479675b3faa52d42f2b2306f14ed3cc3
Flora Peng <berry1003@outlook.com>
2021-02-13 13:55:20 +0800

    feat: modify seeds

--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,19 +1,14 @@
-const mongoose = require('mongoose')
 const URL = require('../url')
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
   URL.create(
     {
-      originalUrl: 'https://www.google.com',
+      originalUrl: 'https://www.google.com/',
       url: 'jEw32'
     },
     {
-      originalUrl: 'http://www.facebook.com',
+      originalUrl: 'https://www.facebook.com/',
       url: '0df2G'
 
     })
