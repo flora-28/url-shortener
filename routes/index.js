5723cb20fc3034e36e8b2fe6e2ab9a46ad14f36c
Flora Peng <berry1003@outlook.com>
2021-02-19 13:50:09 +0800

    feat: modify router.post

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ const express = require('express')
 const router = express.Router()
 const URL = require('../models/url')
 
-if (process.env.NODE_ENV === "production") {
-  commonUrl = 'https://radiant-reaches-97705.herokuapp.com/'
-} else {
-  commonUrl = 'http://localhost:3000/'
-}
-
 //Create random URL
 function getRandomString(length) {
   let result = ""
@@ -17,33 +11,34 @@ function getRandomString(length) {
   return result
 }
 
-//Prevent duplicate URL
-function preventDuplicate(url, res) {
-  URL.exists({ url })
-    .then((duplicate) => {
-      if (duplicate) {
-        return preventDuplicate(getRandomString(5))
-      } else {
-        URL.create({ originalUrl, url })
-        shortenedUrl = commonUrl + url
-        return shortenedUrl
-      }
-    })
-    .then((shortenedUrl) => res.render('result', { shortenedUrl }))
-}
-
 router.get('/', (req, res) => {
   res.render('index')
 })
 
 router.post('/', (req, res) => {
-  originalUrl = req.body.url
-  let randomUrl = getRandomString(5)
-  preventDuplicate(randomUrl, res)
+  let originalUrl = req.body.originalUrl
+  let commonUrl = ''
+  if (process.env.NODE_ENV === "production") {
+    commonUrl = 'https://radiant-reaches-97705.herokuapp.com/'
+  } else {
+    commonUrl = 'http://localhost:3000/'
+  }
+  let url = getRandomString(5)
+  let shortenedUrl = commonUrl + url
+  URL.exists({ url })
+    .then((duplicate) => {
+      if (duplicate) {
+        url = getRandomString(5)
+      } else {
+        URL.create({ originalUrl, url })
+        return shortenedUrl
+      }
+    })
+    .then((shortenedUrl) => res.render('result', { shortenedUrl }))
 })
 
 router.get('/:url', (req, res) => {
-  const url = req.params.url
+  let url = req.params.url
   URL.find({ url })
     .lean()
     .then((link) => {
